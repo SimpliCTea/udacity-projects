45b0cbdc4032c0df5fffaf812c0b405508a9c05c
Marc-Leon Römer <marcleonroemer@gmx.net>
2019-06-07 11:48:21 +0200

    feat: add python script based on etl-pipeline notebook

--- disaster_response_pipeline/data/process_data.py
+++ disaster_response_pipeline/data/process_data.py
@@ -1,16 +1,67 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """loads the data from given paths and returns it in a pandas.DataFrame
+    
+    Arguments:
+        messages_filepath {string} -- path to the message data
+        categories_filepath {string} -- path to the categories data
+    
+    Returns:
+        pandas.DataFrame -- a dataframe holding both messages and their categories, merged by using their id
+    """
+    messages = pd.read_csv(messages_filepath, engine='python')
+    categories = pd.read_csv(categories_filepath, engine='python')
+    return messages.merge(categories, on='id')
 
 
 def clean_data(df):
-    pass
+    """Cleans dataframe holding disaster messages from duplicates, splits categories column and updates column labels
+    
+    Arguments:
+        df {pandas.DataFrame} -- dataframe holding disaster response messages and their categories
+    
+    Returns:
+        pandas.DataFrame -- dataframe with updated column labels, no duplicates and category columns
+    """
+    categories = df['categories'].str.split(pat=';', expand=True)
+    
+    # update column labels
+    row = categories.iloc[0]
+    category_colnames = row.apply(lambda x: x[0:-2])
+    categories.columns = category_colnames
+
+    # split categories into columns
+    for column in categories:
+        categories[column] = categories[column].str[-1]
+        categories[column] = pd.to_numeric(categories[column])
+    df = pd.concat([df.drop(columns='categories'), categories], axis=1)
+
+    # remove duplicates
+    print('{} duplicate ids found. Removing them now...'.format(df[df.duplicated(subset='id')].shape[0]))
+    df.drop_duplicates(subset='id', inplace=True)
+
+    return df
 
 
-def save_data(df, database_filename):
-    pass  
+def save_data(df, database_filename, table_name='disaster_messages'):
+    """Saves dataframe to given sqlite database
+    
+    Arguments:
+        df {pandas.DataFrame} -- a pandas dataframe
+        database_filename {string} -- name of a sqlite database the dataframe should be saved to
+        table_name {string} -- name of the table the dataframe should be saved as; default: 'disaster_messages'
+    
+    Returns:
+        None
+    """
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql(table_name, engine, index=False)
+
+    return None
 
 
 def main():
